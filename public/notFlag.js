f956a898b2386150c48c979d2ecb53d75e3751ff
tuandat732 <spt.may7320@gmail.com>
2020-01-18 10:15:25 +0700

    update do kho

--- public/notFlag.js
+++ public/notFlag.js
@@ -5,6 +5,5 @@ flag.addEventListener('change',()=>{
     if(ans=='yes') alert("ohh, i dont like this game");
     else if(ans=='no'){
          alert("You dont like, but a make this game for youuuu gru gru. But now i am very happy then...the game is end if you have 1000 point");       
-    }
-    else alert("không biết đọc à, nhập cho đúng đi")
+    }else alert("Không biết đọc à, nhập cho đúng đi")
 })
